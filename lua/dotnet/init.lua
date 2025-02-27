4da04344aa7142ce04077227a2761f3ae1fe86ad
James Mattner <james.mattner@gmail.com>
2025-02-27 17:02:47 +0800

    parse and give read access to build artifacts

--- lua/dotnet/init.lua
+++ lua/dotnet/init.lua
@@ -9,20 +9,54 @@ local WindowConfigs = {
     SIDE = "side",
 }
 
+---@type table<string, string>
+local artifact_paths = {}
+
+---@param output string
+local parse_build_artifact = function(output)
+    if not output then return end
+    for line in output:gmatch("[^\r\n]+") do
+        local name, path = line:match('%s*(.-)%s+->%s+(.+%.dll)')
+        if not name or not path then
+            name, path = line:match('%s*(.-)%s+→%s+(.+%.dll)')
+        end
+        if name and path then
+            artifact_paths[name] = path
+        end
+    end
+end
+
 ---@param drawer_name string
----@param cmd string
-local run = function(drawer_name, cmd)
-    local append = function(_, data)
+---@return fun(data: string)
+local get_append_handler = function(drawer_name)
+    return function(data)
         drawer.with_drawer(drawer_name, function(bufnr)
             buffer.append(bufnr, data)
         end)
     end
+end
 
-    append(nil, "=== running: " .. cmd)
+---@param cmd string
+---@param stdout_handlers fun(data: string)[]
+---@param stderr_handlers fun(data: string)[]
+local run = function(cmd, stdout_handlers, stderr_handlers)
+    local handle_stdout = function(_, data)
+        for _, handler in ipairs(stdout_handlers) do
+            handler(data)
+        end
+    end
+
+    local handle_stderr = function(_, data)
+        for _, handler in ipairs(stderr_handlers) do
+            handler(data)
+        end
+    end
+
+    handle_stdout(nil, "❯ " .. cmd)
     vim.system({ "powershell", "-Command", cmd }, {
         text = true,
-        stdout = append,
-        stderr = append
+        stdout = handle_stdout,
+        stderr = handle_stderr
     })
 end
 
@@ -42,25 +76,31 @@ M.setup = function(_)
     drawer.create(BUILD_DRAWER_NAME, bufnr, configs)
 end
 
----@return string the path to the produced artefact
 M.build = function()
-    drawer.show(BUILD_DRAWER_NAME, WindowConfigs.BOTTOM) -- TODO - default value for focus?
-    run(BUILD_DRAWER_NAME, "dotnet build")
-    return "my/artefact/path"                            -- TODO - get this path from the build output
+    drawer.show(BUILD_DRAWER_NAME, WindowConfigs.BOTTOM)
+    local append_handler = get_append_handler(BUILD_DRAWER_NAME)
+    run("dotnet build", { parse_build_artifact, append_handler }, { append_handler })
 end
 
 M.clean = function()
     drawer.show(BUILD_DRAWER_NAME, WindowConfigs.BOTTOM)
-    run(BUILD_DRAWER_NAME, "dotnet clean")
+    local append_handler = get_append_handler(BUILD_DRAWER_NAME)
+    run("dotnet clean", { append_handler }, { append_handler })
 end
 
 M.restore = function()
     drawer.show(BUILD_DRAWER_NAME, WindowConfigs.BOTTOM)
-    run(BUILD_DRAWER_NAME, "dotnet restore")
+    local append_handler = get_append_handler(BUILD_DRAWER_NAME)
+    run("dotnet restore", { append_handler }, { append_handler })
 end
 
 M.toggle = function()
     drawer.toggle_visibility(BUILD_DRAWER_NAME)
+end
+
+---@return table<string, string> a dictionary of project names to artifact paths generated in this session
+M.getArtifactPaths = function()
+    return vim.deepcopy(artifact_paths)
 end
 
 return M
